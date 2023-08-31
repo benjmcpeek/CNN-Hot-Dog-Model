bb22a5940dba0902685d379e2eee45e02b17d4b8
Joey Notaro <joey.notaro@gmail.com>
2023-08-31 13:44:34 -0700

    working to get streamlit connected to model. Problems with shape.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 from operator import mod
 import streamlit as st
+from PIL import Image
+from tensorflow.keras.utils import image_dataset_from_directory, img_to_array
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Rescaling
+from tensorflow.keras.regularizers import l1, l2, l1_l2
+from tensorflow.keras.saving import load_model
+from tensorflow.keras.callbacks import EarlyStopping
 import pickle
+import numpy as np
+
+
 
 st.title('Classification on Images of Hot Dogs')
 st.write("""
@@ -9,28 +19,27 @@ to decipher whether or not an image contains a hot dog or not.
 Please upload a 'jpg' or 'png' image of your choosing below to have the model determine 
 if your image includes a hot dog or not!""")
 
-# Below code copied from 9.06 lesson GA
 
-def load_model():
-  with open('./models_copy/model_base.pkl', 'rb') as f:
-    the_model = pickle.load(f)
-  return the_model
-
-model = load_model()
-
-
-image = Image.open('1502.jpg')
+image = Image.open('./hotdog_nothotdog/hotdog/1502.jpg')
 st.image(image, caption= 'Example of image with hotdog(s) taken from test set of model')
 
-image = Image.open('164.jpg')
+image = Image.open('./hotdog_nothotdog/nothotdog/164.jpg')
 st.image(image, caption= 'Example of image without hotdog(s) taken from test set of model')
 
 img_data = st.file_uploader(label='Load an image:', type = ['jpg', 'png'])
 if img_data is not None:
     uploaded_img = Image.open(img_data)
+    new_img = uploaded_img.resize((299, 299))
+    other_new_img = np.expand_dims(new_img, axis = 0)
     st.image(uploaded_img)
+    img = img_to_array(other_new_img)
+    model = load_model('./models/model_3.h5')
+    # print(model.predict(img))
+    # model.history['val_loss'][-1], model.history['val_acc'][-1]
+    st.write(f'Your image is a {model.predict(img)}')
 
+    
 # code above found at https://discuss.streamlit.io/t/drag-and-drop-image/43144
-st.write(uploaded_img)
+# st.write(uploaded_img)
 
 
